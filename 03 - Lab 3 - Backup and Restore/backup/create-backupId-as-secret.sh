50850b1934c9c76cadee0854946d2565d5084080
shaarmann <stephan.haarmann@camunda.com>
2025-01-17 11:35:53 +0100

    Ran ShellCheck
    - executed ShellCheck and corrected all findings:

--- 03 - Lab 3 - Backup and Restore/backup/create-backupId-as-secret.sh
+++ 03 - Lab 3 - Backup and Restore/backup/create-backupId-as-secret.sh
@@ -6,12 +6,12 @@ backupTimeId=$(date +"%Y%m%d%H%M%S")
 # Check if the secret exists
 kubectl get secret backup-timeid >/dev/null 2>&1
 
-if [ "$(kubectl get secret backup-timeid >/dev/null 2>&1)"]; then
+if kubectl get secret backup-timeid >/dev/null 2>&1; then
     # Secret exists, update it
-    kubectl create secret generic backup-timeid --from-literal=backupTimeId=$backupTimeId --dry-run=client -o yaml | kubectl apply -f -
+    kubectl create secret generic backup-timeid --from-literal=backupTimeId="$backupTimeId" --dry-run=client -o yaml | kubectl apply -f -
     echo "Updated secret with backupTimeId: $backupTimeId"
 else
     # Secret does not exist, create it
-    kubectl create secret generic backup-timeid --from-literal=backupTimeId=$backupTimeId
+    kubectl create secret generic backup-timeid --from-literal=backupTimeId="$backupTimeId"
     echo "Created secret with backupTimeId: $backupTimeId"
 fi
